3e8135e682525ea6f30082c2ae0a3069a4322aad
sammy saqfelhait <ssaqfelhait@student.cscc.edu>
2026-04-16 17:30:26 -0400

    p2  modified create fleet to also create all the rows

--- alien_fleet.py
+++ alien_fleet.py
@@ -20,30 +20,42 @@ class AlienFleet:
 
     def create_fleet(self) -> None:
         alien_w = self.settings.alien_w
+        aline_h = self.settings.alien_h
         screen_w = self.settings.screen_w
+        screen_h = self.settings.screen_h
 
-        fleet_w = self.calculate_fleet_size(alien_w, screen_w)
+        fleet_w, fleet_h = self.calculate_fleet_size(alien_w, screen_w, aline_h)
 
-        # half_screen = self.settings.screen_w
+         half_screen = self.settings.screen_w // 2
         fleet_horizontal_space = fleet_w * alien_w
+        fleet_vertical_space = fleet_h * aline_h
         x_offset = int((screen_w - fleet_horizontal_space) // 2)
-
+        y_offset = int(half_screen - fleet_vertical_space // 2)
+for row in range(fleet_h):
+            current_y = aline_h * row + y_offset
         for col in range(fleet_w):
             current_x = alien_w * col + x_offset
             if col % 2 == 0:
                 continue
 
-            self._create_alien(current_x, 10)
+            self._create_alien(current_x,current_y)
 
-    def calculate_fleet_size(self, alien_w: int, screen_w: int ) -> int:
+    def calculate_fleet_size(self, alien_w: int, screen_w: int, alien_h: int) -> int:
         fleet_w = (screen_w // alien_w)
+        screen_h = self.settings.screen_h
+        fleet_h = (screen_h // alien_h) // 2
 
-        if fleet_w % 2 == 0:
+        if fleet_w % 2 == 0 or row % 2 == 0:
             fleet_w -= 1
         else:
             fleet_w -= 2
 
-        return fleet_w
+            if fleet_h % 2 == 0:
+                fleet_h -= 1
+                else:
+                    fleet_h -= 2
+
+        return int(fleet_w), int(fleet_h)
     
 
     def _create_alien(self, current_x, current_y) -> None:
